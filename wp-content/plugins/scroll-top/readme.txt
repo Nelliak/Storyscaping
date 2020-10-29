b132e52001475881e4416426df47c14d36c24b2e
Nelliak <eaapeva@students.eaaa.dk>
2020-10-29 21:26:43 +0100

    29.10 prefinal
    almost final from domain(web)

---
+++ wp-content/plugins/scroll-top/readme.txt
@@ -0,0 +1,61 @@
+=== ID Back To Top ===
+Contributors: idenovasi, satrya
+Tags: back to top, button, to top, jquery, scroll to top, scroll top, customization
+Requires at least: 4.8
+Tested up to: 5.5
+Stable tag: 1.1.0
+Requires PHP: 5.2.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automaticlly adds a flexible Back to Top button to your WordPress website that allows your visitor to scroll back to the top of your page with one click!
+
+== Description ==
+
+This plugin will automaticlly enable a custom and flexible **Back to Top** button to your WordPress website that allows your visitor to scroll back to the top of your page with one click!
+
+= Features Include: =
+
+* Free FOREVER.
+* No setup needed.
+* **Custom target.**
+* Unlimited colors.
+* Retina support(icon font).
+* Choose text or icon font.
+* Customizable text.
+* Position switcher (left or right).
+* Change animation you like.
+* Custom CSS area.
+
+**Contribute on [Github](https://github.com/idenovasi/scroll-top).**
+
+== Installation ==
+
+**Through Dashboard**
+
+1. Log in to your WordPress admin panel and go to Plugins -> Add New
+2. Type **scroll top** in the search box and click on search button.
+3. Find ID Back To Top plugin.
+4. Then click on Install Now after that activate the plugin.
+5. Go to Settings -> Back To Top.
+
+**Installing Via FTP**
+
+1. Download the plugin to your hardisk.
+2. Unzip.
+3. Upload the "scroll-top" folder into your plugins directory.
+4. Log in to your WordPress admin panel and click the Plugins menu.
+5. Then activate the plugin.
+6. Go to Settings -> Back To Top.
+
+== Screenshots ==
+
+1. The plugin settings.
+
+== Changelog ==
+
+= 1.1.0 =
+* Support WordPress 5.5
+* Change name from **Scroll Top** to **ID Back To Top**
+* Better and slimmer back to top icon
+
--- wp-content/plugins/sweet-plugin/index.php
+++ wp-content/plugins/sweet-plugin/index.php
@@ -1,26 +1,38 @@
 <?php
 /*
-* Plugin Name: Sweet plugin
+* Plugin Name: Sweet-plugin
 * Description: This is a sweet plugin for a storyscaping project. The plugin is based on HTML, CSS, JS and PHP
 * Plugin URI: https://miglepdkt.com/storyscaping/wp-content/plugins/sweet-plugin
-* Version: 0.2.2
+* Version: 0.2.8
 * Author: Migle, Nela, Monika
 * Author URI: https://miglepdkt.com/storyscaping/wp-content/plugins/sweet-plugin
 * License: GPL2
 */
 
+
+#First parameter is a self choosen name for a unique short-code. Second parameter is the name of the function that creates the popup
+add_shortcode('show_sweet_popup','sweet_popup');
+
+# Take action - activate it
+add_action('wp_enqueue_scripts','register_styles_and_scripts_for_sweet_popup');
+
+
 # Creating a sweet popup
 function sweet_popup()
 {
 $content = '';
+$content .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js">';
+$content .= '<script src="https://kit.fontawesome.com/90f11e08e9.js" crossorigin="anonymous"></script>';
+$content .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>';
+$content .= '<script src="./script.js"></script>';
 $content .= '<div id="overlay" class="cover blur-in">';
 $content .= '</div>';
 $content .= '<div class="row pop-up">';
 $content .= '<div class="box small-6 large-centered">';
 $content .= '<a href="#" class="close-button">✖</a>';
 $content .= '<h3>#growwiththlangs</h3>';
-$content .= '<p>Share moments at school on your Instagram by using hashtag #growwiththlangs. </p>';
-$content .= '<p> "Be happy for this moment. This moment is your life." </p>';
+$content .= '<p class="animate__animated animate__bounceInLeft">Share moments at school on your Instagram by using hashtag #growwiththlangs.</p>';
+$content .= '<p class="animate__animated animate__bounceInRight"> "Be happy for this moment. This moment is your life." </p><i style="font-size: 48px; color: Dodgerblue;"class="fab fa-facebook"></i><i style="font-size: 48px; color: #E1306C; margin-left:10px;" class="fab fa-instagram"></i>';
 $content .= '</div>';
 $content .= '</div>';
 
@@ -28,12 +40,6 @@ return $content;
 
 }
 
- #First parameter is a self choosen name for a unique short-code. Second parameter is the name of the function that creates the popup
-
-add_shortcode('show_sweet_popup','sweet_popup');
-
-# Take action - activate it
-    add_action('wp_enqueue_scripts','register_styles_and_scripts_for_sweet_popup');
 
 function register_styles_and_scripts_for_sweet_popup() 
     {
@@ -41,11 +47,7 @@ function register_styles_and_scripts_for_sweet_popup()
     wp_enqueue_style('CSSfaundation','https://cdn.jsdelivr.net/foundation/5.3.3/css/foundation.css');
     wp_enqueue_style('MyCSS',plugins_url('sweet-plugin/style.css'));
    
-  wp_deregister_script('jquery');
-  wp_enqueue_script('jquery','https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js',array(),null,true);
-   
-   
-   
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+    wp_enqueue_script('CustomScript', plugins_url('sweet-plugin/script.js'), array('jquery'), null, true);
     }
-
-?>
